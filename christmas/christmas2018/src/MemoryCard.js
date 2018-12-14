9067eb4f14d310687003a1c9979027c94bff7b79
Hyun Joo Nam <hyunjoooo.nam@gmail.com>
2018-12-14 01:05:10 -0500

    1214 error

--- christmas/christmas2018/src/MemoryCard.js
+++ christmas/christmas2018/src/MemoryCard.js
@@ -7,17 +7,21 @@ class MemoryCard extends Component {
     constructor() { 
         super();
         this.state = {
-            isFlipped=false
+            isFlipped: false
         }
     };
     clickHandler() {
-        this.setState({isFlipped:true});
+        this.setState({isFlipped: true});
     }  
 
     render() {
+        const memoryCardInnerClass = "memoryCardInner";
+        if (this.state.isFlipped === true) {
+            memoryCardInnerClass += " flipped"
+        }
         return(
             <div className="MemoryCard" onClick={this.clickHandler}>
-                <div className="MemoryCardInner">
+                <div className={memoryCardInnerClass}>
                     <div className="MemoryCardBack">
                         <img src="https://images.emojiterra.com/google/android-pie/512px/1f436.png" />
                     </div>
